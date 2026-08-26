f8947a902c11ff6469c0a8566cfe2784c447d6ad
lucatak <contato.lucas.santos@hotmail.com>
2026-08-26 18:28:00 -0300

    adiciona exercicio

--- p1/aula3/codigo-prof/produtos.php
+++ p1/aula3/codigo-prof/produtos.php
@@ -1,16 +1,26 @@
 <?php
-$conteudo = file_get_contents( 'produtos.csv' );
-$linhas = explode( "\n", $conteudo );
-array_shift( $linhas ); // Remove a 1a linha - títulos
-array_pop( $linhas ); // Remove a última linha (vazia)
-// var_dump( $linhas );
-
 // Exercício: calcular o Inventário do estoque
 // Inventário: multiplicação do estoque pelo preço, em todos os produtos (soma)
 
+$conteudo = file_get_contents( 'produtos.csv' );
+
+$linhas = explode( "\n", $conteudo );
+
+array_shift( $linhas );
+array_pop( $linhas );
+
+$inventario = 0;
+
 foreach ( $linhas as $l ) {
+
     $partes = explode( ';', $l );
+
     $preco = trim( $partes[ 3 ], ' R$' );
     $preco = str_replace( ',', '.', $preco );
+
     $estoque = $partes[ 2 ];
+
+    $inventario += $estoque * $preco;
 }
+
+echo "Inventário: R$ $inventario\n";
